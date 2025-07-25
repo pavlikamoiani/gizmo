eec6af689a79d8a5d7f6ee1a02a19a2a7812b694
pavlik <107913419+iceone7@users.noreply.github.com>
2025-07-25 21:37:34 +0400

    Increase max-width of slider images from 340px to 350px for better display and adjust database connection comments for clarity.

--- db/db.php
+++ db/db.php
@@ -10,3 +10,10 @@ if ($conn->connect_error) {
     die('Connection failed: ' . $conn->connect_error);
 }
 ?>
+
+
+<!-- $host = 'localhost';
+$dbname = 'gizmocomge475_gizmo'; 
+$user = 'gizmocomge475_admin'; 
+$password = 'kY82La1deohdrZ'; -->
+<!-- kY82La1deohdrZ -->
